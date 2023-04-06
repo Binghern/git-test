e43e2d570fd8052c6f2b1dbb32cac6f8d438fc01
Binnhenn <binnhenn@126.com>
2023-04-06 22:27:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Description:
+ * @Author:
+ * @Version:
+ * @Date: 2023-04-06 22:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
